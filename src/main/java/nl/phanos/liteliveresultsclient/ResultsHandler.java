744c57ebf473849618afb5a4b99e14d954df9cea
w.kievit <w.kievit@NB0161.nki.nl>
2017-02-10 15:27:31 +0100

    init

---
+++ src/main/java/nl/phanos/liteliveresultsclient/ResultsHandler.java
@@ -0,0 +1,78 @@
+package nl.phanos.liteliveresultsclient;
+
+import nl.phanos.liteliveresultsclient.classes.ParFile;
+import nl.phanos.liteliveresultsclient.gui.Main;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author woutermkievit
+ */
+public class ResultsHandler extends Thread {
+
+    private File dir;
+    private ArrayList<ParFile> files = new ArrayList<ParFile>();
+
+    public ResultsHandler(File dir) {
+        this.dir = dir;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("start reading");
+            for (File fileEntry : dir.listFiles()) {
+                if (fileEntry.getName().endsWith("txt")) {
+                    ParFile parFile = AtletiekNuPanel.panel.parFiles.get(fileEntry.getName().replace("txt", "par"));
+                    if (parFile != null && parFile.resultSize != fileEntry.length()) {
+                        parFile.resultFile = fileEntry;
+                        parFile.gotResults = true;
+                        files.add(parFile);
+                    }
+                }
+            }
+            try {
+                if (AtletiekNuPanel.panel.live && files.size() > 0&&AtletiekNuPanel.panel.getClass()==AtletiekNuPanel.class){
+                    ((AtletiekNuPanel)AtletiekNuPanel.panel).loginHandler.submitResults(files);
+                    System.out.println("upload");
+                }
+                files.clear();
+            } catch (Exception ex) {
+                System.out.println("failed to upload");
+                Logger.getLogger(ResultsHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            AtletiekNuPanel.panel.UpdateList();
+            System.out.println("end reading");
+            //if(!ResultsPanel.panel.test){
+            //}
+            try {
+                sleep(10000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ResultsHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+}
